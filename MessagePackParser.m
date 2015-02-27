69ea6a94592578620e2a60b29070a59b0539839a
Tae Won Ha <h@taewon.de>
2015-02-27 19:12:36 +0100

    remove unnec' break

--- MessagePackParser.m
+++ MessagePackParser.m
@@ -16,35 +16,25 @@
   switch (obj.type) {
     case MSGPACK_OBJECT_BOOLEAN:
       return [[NSNumber alloc] initWithBool:obj.via.boolean];
-      break;
     case MSGPACK_OBJECT_POSITIVE_INTEGER:
       return [[NSNumber alloc] initWithUnsignedLongLong:obj.via.u64];
-      break;
     case MSGPACK_OBJECT_NEGATIVE_INTEGER:
       return [[NSNumber alloc] initWithLongLong:obj.via.i64];
-      break;
     case MSGPACK_OBJECT_FLOAT:
       return [[NSNumber alloc] initWithDouble:obj.via.f64];
-      break;
     case MSGPACK_OBJECT_STR:
       return [[NSString alloc] initWithBytes:obj.via.str.ptr length:obj.via.str.size encoding:NSUTF8StringEncoding];
-      break;
     case MSGPACK_OBJECT_BIN:
       return [[NSData alloc] initWithBytes:obj.via.bin.ptr length:obj.via.bin.size];
-      break;
     case MSGPACK_OBJECT_ARRAY: {
       NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity:obj.via.array.size];
       msgpack_object *const pend = obj.via.array.ptr + obj.via.array.size;
       for (msgpack_object *p = obj.via.array.ptr; p < pend; p++) {
         id newArrayItem = [self createUnpackedObject:*p];
         [arr addObject:newArrayItem];
-#if !__has_feature(objc_arc)
-                [newArrayItem release];
-#endif
       }
       return arr;
     }
-      break;
     case MSGPACK_OBJECT_MAP: {
       NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithCapacity:obj.via.map.size];
       msgpack_object_kv *const pend = obj.via.map.ptr + obj.via.map.size;
@@ -52,18 +42,12 @@
         id key = [self createUnpackedObject:p->key];
         id val = [self createUnpackedObject:p->val];
         [dict setValue:val forKey:key];
-#if !__has_feature(objc_arc)
-				[key release];
-				[val release];
-#endif
       }
       return dict;
     }
-      break;
     case MSGPACK_OBJECT_NIL:
     default:
       return [NSNull null]; // Since nsnull is a system singleton, we don't have to worry about ownership of it
-      break;
   }
 }
 
